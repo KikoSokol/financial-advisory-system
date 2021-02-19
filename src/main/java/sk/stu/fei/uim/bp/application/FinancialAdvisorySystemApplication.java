aff9fc966b341d86f9fbca727780e96bceab1add
Kristián Sokol <kikosokol@gmail.com>
2021-02-19 23:28:17 +0100

    Initial commit

---
+++ src/main/java/sk/stu/fei/uim/bp/application/FinancialAdvisorySystemApplication.java
@@ -0,0 +1,13 @@
+package sk.stu.fei.uim.bp.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAdvisorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAdvisorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/stu/fei/uim/bp/application/FinancialAdvisorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.stu.fei.uim.bp.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAdvisorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
