c56b8f4fd951e6dbb6b171e4c80185ead924eaf9
Kristián Sokol <kikosokol@gmail.com>
2021-02-21 22:22:13 +0100

    Pridal som: + DatabaseConfiguration - konfiguračnú triedu pre nakonfigurovanie spojenia s databázou pre aplikáciu
    + DatabaseConfigurationForTest - konfiguračnú triedu pre nakonfigurovanie spojenia s databázou pre testy
    
    + nastavil som profily main - pre aplikáciu a v testoch profil test pre testovanie

---
+++ src/main/java/sk/stu/fei/uim/bp/application/configuration/DatabaseConfiguration.java
@@ -0,0 +1,54 @@
+package sk.stu.fei.uim.bp.application.configuration;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+
+
+@Configuration
+@Profile("main")
+public class DatabaseConfiguration
+{
+
+    private final MappingMongoConverter mappingMongoConverter;
+
+    @Autowired
+    public DatabaseConfiguration(MappingMongoConverter mappingMongoConverter)
+    {
+        this.mappingMongoConverter = mappingMongoConverter;
+    }
+
+    @Bean
+    public MongoClient mongoClient()
+    {
+        return MongoClients.create("mongodb://localhost:27017");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate()
+    {
+        return new MongoTemplate(mongoClient(),"financial_advisory_system");
+    }
+
+    @Bean
+    public GridFsTemplate gridFsTemplate()
+    {
+        return new GridFsTemplate(mongoDbFactory(),mappingMongoConverter);
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoDbFactory()
+    {
+        return new SimpleMongoClientDatabaseFactory(mongoClient(),"financial_advisory_system");
+    }
+
+
+}
--- src/test/java/sk/stu/fei/uim/bp/application/FinancialAdvisorySystemApplicationTests.java
+++ src/test/java/sk/stu/fei/uim/bp/application/FinancialAdvisorySystemApplicationTests.java
@@ -1,13 +1,17 @@
 package sk.stu.fei.uim.bp.application;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class FinancialAdvisorySystemApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads()
+	{
 	}
 
 }
