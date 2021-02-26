a217d9f0861543435b116e42fa1ff764e015566f
Kristián Sokol <kikosokol@gmail.com>
2021-02-26 23:56:02 +0100

    Pridal som: + FileRepository ktoré služia na načítavanie a ukladanie súborov v aplikácií

---
+++ src/main/java/sk/stu/fei/uim/bp/application/backend/file/FileRepository.java
@@ -0,0 +1,23 @@
+package sk.stu.fei.uim.bp.application.backend.file;
+
+import com.mongodb.DBObject;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import com.vaadin.flow.component.upload.receivers.FileData;
+import org.bson.types.ObjectId;
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface FileRepository
+{
+    ObjectId saveFile(InputStream file, FileData fileData, DBObject metadata);
+
+    ObjectId saveFile(InputStream file, FileData fileData);
+
+    ObjectId saveFile(InputStream file, FileData fileData, String customFileName, DBObject metadata);
+
+    ObjectId saveFile(InputStream file, FileData fileData, String customFileName);
+
+    GridFSFile getInformationAboutFile(ObjectId fileId);
+
+    InputStream getFile(ObjectId fileId) throws IOException;
+}
---
+++ src/main/java/sk/stu/fei/uim/bp/application/backend/file/FileRepositoryImpl.java
@@ -0,0 +1,80 @@
+package sk.stu.fei.uim.bp.application.backend.file;
+
+import com.mongodb.DBObject;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import com.vaadin.flow.component.upload.receivers.FileData;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Repository
+public class FileRepositoryImpl implements FileRepository
+{
+    private final GridFsOperations gridFsOperations;
+
+    @Autowired
+    public FileRepositoryImpl(GridFsTemplate gridFsTemplate)
+    {
+        this.gridFsOperations = gridFsTemplate;
+    }
+
+    @Override
+    public ObjectId saveFile(InputStream file, FileData fileData, DBObject metadata)
+    {
+
+        return this.gridFsOperations.store(file,fileData.getFileName(),fileData.getMimeType(),metadata);
+    }
+
+    @Override
+    public ObjectId saveFile(InputStream file, FileData fileData)
+    {
+
+        return this.gridFsOperations.store(file,fileData.getFileName(),fileData.getMimeType());
+    }
+
+    @Override
+    public ObjectId saveFile(InputStream file, FileData fileData, String customFileName,DBObject metadata)
+    {
+        String name = createCustomFileName(fileData.getFileName(),customFileName);
+        return this.gridFsOperations.store(file,name,fileData.getMimeType(),metadata);
+    }
+
+    @Override
+    public ObjectId saveFile(InputStream file, FileData fileData, String customFileName)
+    {
+        String name = createCustomFileName(fileData.getFileName(),customFileName);
+        return this.gridFsOperations.store(file,name,fileData.getMimeType());
+    }
+
+    @Override
+    public GridFSFile getInformationAboutFile(ObjectId fileId)
+    {
+        Criteria criteria = new Criteria("_id");
+        criteria.is(fileId);
+
+        Query query = new Query(criteria);
+
+        return this.gridFsOperations.findOne(query);
+    }
+
+    @Override
+    public InputStream getFile(ObjectId fileId) throws IOException {
+        GridFSFile fileInfo = getInformationAboutFile(fileId);
+
+        return this.gridFsOperations.getResource(fileInfo).getInputStream();
+    }
+
+    private String createCustomFileName(String oldFileName, String newFileName)
+    {
+        int dotPossition = oldFileName.lastIndexOf('.');
+
+        return newFileName.concat(oldFileName.substring(dotPossition));
+    }
+}
