f1b1ba02943df86721329ebe367b3b80409dfe1a
Kristián Sokol <kikosokol@gmail.com>
2021-04-17 03:46:34 +0200

    Pridal som: + package utils + v package utils som vztvoril objekt ktorý bude mať informácie o prílohach (súboroch)

---
+++ src/main/java/sk/stu/fei/uim/bp/application/utils/ContractFileAttachment.java
@@ -0,0 +1,26 @@
+package sk.stu.fei.uim.bp.application.utils;
+
+
+import org.bson.types.ObjectId;
+
+import javax.validation.constraints.NotNull;
+
+public class ContractFileAttachment extends FileAttachment
+{
+    @NotNull
+    private int contractVersion;
+
+    public ContractFileAttachment(ObjectId fileInDbId, String fileName, int contractVersion) {
+        super(fileInDbId, fileName);
+        this.contractVersion = contractVersion;
+    }
+
+
+    public int getContractVersion() {
+        return contractVersion;
+    }
+
+    public void setContractVersion(int contractVersion) {
+        this.contractVersion = contractVersion;
+    }
+}
---
+++ src/main/java/sk/stu/fei/uim/bp/application/utils/FileAttachment.java
@@ -0,0 +1,33 @@
+package sk.stu.fei.uim.bp.application.utils;
+
+import com.mongodb.client.gridfs.model.GridFSFile;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@Document("FileAttachment")
+public class FileAttachment
+{
+    @MongoId
+    private ObjectId fileAttachmentId;
+
+    @NotNull
+    private ObjectId fileInDbId;
+
+    @Transient
+    private GridFSFile file;
+
+    @NotNull
+    private String fileName;
+
+
+    public FileAttachment(ObjectId fileInDbId, String fileName) {
+        setFileInDbId(fileInDbId);
+        setFileName(fileName);
+    }
+}
